af47cccf93f59eb2bac7afdab334707a770da7ed
Keli Hlodversson <keli.hlodversson@laerdal.com>
2026-08-21 13:15:24 +0200

    Improve EmuCON contract diagnostics

--- tools/test-emucon-ctrl-arrows.sh
+++ tools/test-emucon-ctrl-arrows.sh
@@ -8,11 +8,41 @@ fi
 
 repo_root=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 
-rg -q '^#define CTRL_ARROW_LEFT[[:space:]]+0x7300$' "$repo_root/include/scancode.h"
-rg -q '^#define CTRL_ARROW_RIGHT[[:space:]]+0x7400$' "$repo_root/include/scancode.h"
-rg -U -q 'case CTRL_ARROW_LEFT:[\s\S]*word = 1;[\s\S]*case ARROW_LEFT:' "$repo_root/cli/cmdedit.c"
-rg -U -q 'case CTRL_ARROW_RIGHT:[\s\S]*word = 1;[\s\S]*case ARROW_RIGHT:' "$repo_root/cli/cmdedit.c"
-! rg -q 'case SHIFT_ARROW_LEFT:|case SHIFT_ARROW_RIGHT:' "$repo_root/cli/cmdedit.c"
-rg -q 'control-left/right arrow = previous/next word' "$repo_root/cli/cmdint.c"
+require_file()
+{
+    if ! rg -q "$1" "$2"; then
+        echo "missing EmuCON Ctrl-arrow contract: $3"
+        exit 1
+    fi
+}
+
+require_multiline()
+{
+    if ! rg -U -q "$1" "$2"; then
+        echo "missing EmuCON Ctrl-arrow contract: $3"
+        exit 1
+    fi
+}
+
+require_absent()
+{
+    if rg -q "$1" "$2"; then
+        echo "unexpected EmuCON Ctrl-arrow contract: $3"
+        exit 1
+    fi
+}
+
+require_file '^#define CTRL_ARROW_LEFT[[:space:]]+0x7300$' \
+    "$repo_root/include/scancode.h" 'Ctrl-left scan code'
+require_file '^#define CTRL_ARROW_RIGHT[[:space:]]+0x7400$' \
+    "$repo_root/include/scancode.h" 'Ctrl-right scan code'
+require_multiline 'case CTRL_ARROW_LEFT:[\s\S]*word = 1;[\s\S]*case ARROW_LEFT:' \
+    "$repo_root/cli/cmdedit.c" 'Ctrl-left word movement'
+require_multiline 'case CTRL_ARROW_RIGHT:[\s\S]*word = 1;[\s\S]*case ARROW_RIGHT:' \
+    "$repo_root/cli/cmdedit.c" 'Ctrl-right word movement'
+require_absent 'case SHIFT_ARROW_LEFT:|case SHIFT_ARROW_RIGHT:' \
+    "$repo_root/cli/cmdedit.c" 'Shift-arrow word movement'
+require_file 'control-left/right arrow = previous/next word' \
+    "$repo_root/cli/cmdint.c" 'Ctrl-arrow help text'
 
 printf '%s\n' 'EmuCON Ctrl-arrow contract test passed'
