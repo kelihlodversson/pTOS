43ea2a470dca41db29f86f8e8fc0bd53d6417635
Keli Hlodversson <keli.hlodversson@laerdal.com>
2026-08-28 09:49:13 +0200

    copyright: remove copied ARM authors

--- include/arch/arm/asm.h
+++ include/arch/arm/asm.h
@@ -3,9 +3,6 @@
  *
  * Copyright (C) 2018-2026 The pTOS development team
  *
- * Authors:
- *  LVL   Laurent Vogel
- *
  * This file is distributed under the GPL, version 2 or at your
  * option any later version.  See doc/license.txt for details.
  */
